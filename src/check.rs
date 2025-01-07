6bf86de97f7c368c8777f4f7334a241a0b90e632
lyuts <lyuts@pm.me>
2025-01-07 17:54:26 -0500

    Implement extendable code for performing checks on the workspace.

--- src/check.rs
+++ src/check.rs
@@ -1,1 +1,36 @@
-fn main() {}
+use anyhow::Context;
+use std::process::{Command, Stdio};
+
+fn stg_exists() -> anyhow::Result<()> {
+    print!("Checking stg binary...");
+    Command::new("stg")
+        .arg("version")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .output()
+        .map(|_| ())
+        .context("stg binary not fond")
+}
+
+
+fn check<F>(fns: &[F]) -> anyhow::Result<()>
+where
+    F: Fn() -> anyhow::Result<()>,
+{
+    for f in fns {
+        match f() {
+            Ok(_) => println!(" Ok"),
+            Err(e) => {
+                println!(" Failed");
+                eprintln!("{}", e);
+                return Err(e);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    check([stg_exists].as_slice())?;
+    Ok(())
+}
